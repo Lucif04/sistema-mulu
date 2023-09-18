f21e8ce07772c9b73d9442179d5d423990ee941b
Luci <lucy.soporte@gmail.com>
2023-09-18 09:52:37 -0300

    DB Actualziada 18-9

--- sistema.sql
+++ sistema.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-09-2023 a las 00:59:46
+-- Tiempo de generación: 18-09-2023 a las 14:50:31
 -- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.0.28
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -43,10 +43,7 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`id_cliente`, `nombre`, `telefono`, `correo`, `provincia`, `localidad`, `direccion`, `cp`) VALUES
-(1, 'Ana Lopez', '978645132', 'ana@gmail.com', 'Buenos Aires', 'Junin', 'Trujillo - Perú', 0),
-(2, 'Maria sanchez', '974561234', 'ana@gmail.com', 'Buenos Aires', 'Junin', 'Trujillo - Perú', 0),
-(4, 'Nuevo Cliente', '97877789', 'ana@gmail.com', 'Buenos Aires', 'Junin', 'Av. san martin n° 342', 0),
-(6, 'Registro de Cliente', '978978', 'ana@gmail.com', 'Buenos Aires', 'Junin', 'Av. Libertad', 0);
+(1, 'Consumidor Final', '0', '0', '0', '0', '0', 0);
 
 -- --------------------------------------------------------
 
@@ -68,29 +65,7 @@ CREATE TABLE `configuracion` (
 --
 
 INSERT INTO `configuracion` (`id`, `nombre`, `telefono`, `email`, `direccion`, `img`) VALUES
-(1, 'Lushu', '2364652498', 'mushu.skin@gmail.com', 'Borges 451', '../upload/logoemp.png');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `detalle_permisos`
---
-
-CREATE TABLE `detalle_permisos` (
-  `id` int(11) NOT NULL,
-  `id_permiso` int(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `detalle_permisos`
---
-
-INSERT INTO `detalle_permisos` (`id`, `id_permiso`, `id_usuario`) VALUES
-(35, 3, 9),
-(36, 4, 9),
-(37, 5, 9),
-(38, 6, 9);
+(1, 'Mulu', '2364652498', 'mushu.skin@gmail.com', 'Borges 451', '../upload/logoemp.jpeg');
 
 -- --------------------------------------------------------
 
@@ -124,24 +99,6 @@ CREATE TABLE `detalle_venta` (
   `total` decimal(10,2) NOT NULL DEFAULT 0.00
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `detalle_venta`
---
-
-INSERT INTO `detalle_venta` (`id`, `id_producto`, `id_venta`, `cantidad`, `descuento`, `precio`, `total`) VALUES
-(1, 1, 1, 10, 200.00, 1500.00, 14800.00),
-(4, 1, 3, 5, 50.00, 1500.00, 7450.00),
-(6, 3, 3, 2, 100.00, 500.00, 900.00),
-(7, 3, 4, 9, 300.00, 500.00, 17700.00),
-(8, 4, 4, 2, 150.00, 3000.00, 5850.00),
-(9, 5, 5, 8, 100.00, 350.00, 2700.00),
-(10, 4, 5, 1, 200.00, 3000.00, 2800.00),
-(11, 1, 6, 1, 50.00, 1500.00, 1450.00),
-(12, 4, 6, 3, 520.00, 3000.00, 8480.00),
-(13, 3, 6, 1, 0.00, 500.00, 500.00),
-(14, 4, 7, 3, 10.00, 3000.00, 8990.00),
-(15, 1, 8, 2, 0.00, 1500.00, 3000.00);
-
 -- --------------------------------------------------------
 
 --
@@ -149,46 +106,12 @@ INSERT INTO `detalle_venta` (`id`, `id_producto`, `id_venta`, `cantidad`, `descu
 --
 
 CREATE TABLE `pedidos` (
-  `idPedido` int(11) NOT NULL,
+  `id_pedido` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `telefono` int(11) NOT NULL,
-  `pedido` varchar(200) NOT NULL,
-  `fecha` date NOT NULL,
-  `vendido` int(11) NOT NULL COMMENT '0 - no / 1 - si'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Volcado de datos para la tabla `pedidos`
---
-
-INSERT INTO `pedidos` (`idPedido`, `nombre`, `telefono`, `pedido`, `fecha`, `vendido`) VALUES
-(29, 'Benja B', 25478963, 'Compra bcii', '2023-09-10', 1),
-(33, 'asdasd', 324234, 'asdsadsa', '2023-09-10', 1),
-(35, 'Lucia Bellome', 2147483647, 'cuadro de fotos', '2023-09-11', 1),
-(36, 'Felipe Lopez', 2147483647, 'rx 190', '2023-09-11', 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `permisos`
---
-
-CREATE TABLE `permisos` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `permisos`
---
-
-INSERT INTO `permisos` (`id`, `nombre`) VALUES
-(1, 'configuración'),
-(2, 'usuarios'),
-(3, 'clientes'),
-(4, 'productos'),
-(5, 'ventas'),
-(6, 'nueva_venta');
+  `pedido` varchar(100) NOT NULL,
+  `fecha` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -211,11 +134,9 @@ CREATE TABLE `producto` (
 --
 
 INSERT INTO `producto` (`codproducto`, `codigo`, `descripcion`, `precioCompra`, `precioVenta`, `stock`, `fecha`) VALUES
-(1, '123456', 'Televisor Lg', 1400.00, 1500.00, 32, '2023-08-04'),
-(3, '97879846', 'Impresora epson L300', 250.00, 600.00, 3, '2023-09-06'),
-(4, '978798', 'Computadora Lenovo', 1500.00, 3000.00, 41, '2023-06-24'),
-(5, '7977989', 'Scanner', 100.00, 450.00, 6, '0000-00-00'),
-(7, '001', 'Medias Addidas', 1000.00, 1500.00, 10, '2023-03-25');
+(1, '01', 'Tapas Comunes (Pack 10U)', 200.00, 700.00, 100, '2023-09-18'),
+(2, '02', 'Tapas Integrales(Pack 10U)', 250.00, 700.00, 100, '2023-09-18'),
+(3, '03', 'Tapas Mixtas(Pack 10U)', 250.00, 800.00, 100, '2023-09-18');
 
 -- --------------------------------------------------------
 
@@ -236,8 +157,7 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`idusuario`, `nombre`, `correo`, `usuario`, `clave`) VALUES
-(1, 'Sistemas Free', 'ana.info1999@gmail.com', 'admin', '21232f297a57a5a743894a0e4a801fc3'),
-(9, 'Maria Sanchez', 'maria@gmail.com', 'maria', '263bce650e68ab4e23f28263760b9fa5');
+(1, 'Mulu', 'mulu.sistema@gmail.com', 'admin', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
@@ -254,20 +174,6 @@ CREATE TABLE `ventas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `ventas`
---
-
-INSERT INTO `ventas` (`id`, `id_cliente`, `total`, `id_usuario`, `fecha`) VALUES
-(1, 1, 16300.00, 1, '2021-08-09 21:01:45'),
-(2, 1, 12000.00, 1, '2021-08-09 21:05:02'),
-(3, 1, 9140.00, 1, '2021-08-09 21:10:23'),
-(4, 1, 23550.00, 1, '2021-08-10 01:09:24'),
-(5, 2, 5500.00, 1, '2021-08-10 01:25:27'),
-(6, 1, 10430.00, 1, '2021-08-10 21:27:09'),
-(7, 1, 8990.00, 9, '2021-08-10 21:31:50'),
-(8, 1, 3000.00, 1, '2023-09-06 17:50:56');
-
---
 -- Índices para tablas volcadas
 --
 
@@ -282,14 +188,6 @@ ALTER TABLE `cliente`
 --
 ALTER TABLE `configuracion`
   ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `detalle_permisos`
---
-ALTER TABLE `detalle_permisos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_permiso` (`id_permiso`),
-  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `detalle_temp`
@@ -311,13 +209,7 @@ ALTER TABLE `detalle_venta`
 -- Indices de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  ADD PRIMARY KEY (`idPedido`);
-
---
--- Indices de la tabla `permisos`
---
-ALTER TABLE `permisos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_pedido`);
 
 --
 -- Indices de la tabla `producto`
@@ -356,12 +248,6 @@ ALTER TABLE `configuracion`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `detalle_permisos`
---
-ALTER TABLE `detalle_permisos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
-
---
 -- AUTO_INCREMENT de la tabla `detalle_temp`
 --
 ALTER TABLE `detalle_temp`
@@ -371,25 +257,19 @@ ALTER TABLE `detalle_temp`
 -- AUTO_INCREMENT de la tabla `detalle_venta`
 --
 ALTER TABLE `detalle_venta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  MODIFY `idPedido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
-
---
--- AUTO_INCREMENT de la tabla `permisos`
---
-ALTER TABLE `permisos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `codproducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `codproducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
@@ -401,18 +281,11 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
 --
-
---
--- Filtros para la tabla `detalle_permisos`
---
-ALTER TABLE `detalle_permisos`
-  ADD CONSTRAINT `detalle_permisos_ibfk_1` FOREIGN KEY (`id_permiso`) REFERENCES `permisos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `detalle_permisos_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`idusuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `detalle_temp`
